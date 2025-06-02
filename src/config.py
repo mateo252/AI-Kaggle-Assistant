35a7b5e37828ab707385cb10dce1461030104e16
Mateusz <m.loza952@gmail.com>
2025-06-02 22:00:29 +0200

    Add a more universal approach to saving paths

--- src/config.py
+++ src/config.py
@@ -2,13 +2,13 @@ from enum import Enum
 import sys, os
 
 
+MAIN_FILE_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+ASSETS_SUBDIR = "assets"
+PAGES_SUBDIR = "pages"
 
 class PathVariable(Enum):
-    MAIN_FILE_PATH = sys.argv[0]
-
-    NOTEBOOK_PATH = os.path.join(MAIN_FILE_PATH, "notebook")
-    SAVE_PATH =     os.path.join(MAIN_FILE_PATH, "sabe")
-    TEMPLATE_PATH = os.path.join(MAIN_FILE_PATH, "template")
-    TMP_PATH =      os.path.join(MAIN_FILE_PATH, "tmp")
-
-
+    NOTEBOOK_PATH = os.path.join(MAIN_FILE_PATH, ASSETS_SUBDIR, "notebook")
+    SAVE_PATH =     os.path.join(MAIN_FILE_PATH, ASSETS_SUBDIR, "save")
+    TEMPLATE_PATH = os.path.join(MAIN_FILE_PATH, ASSETS_SUBDIR, "template")
+    TMP_PATH =      os.path.join(MAIN_FILE_PATH, ASSETS_SUBDIR, "tmp")
+    PAGES_PATH =    os.path.join(MAIN_FILE_PATH, PAGES_SUBDIR)
